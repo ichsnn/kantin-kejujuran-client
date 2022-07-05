426dec2eae9a9c1490ea7f584c5c03f77fb3053f
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-07-05 09:58:12 +0700

    update

--- components/item/ItemCard.js
+++ components/item/ItemCard.js
@@ -1,11 +1,10 @@
 import React from "react";
-import itemImage from "../../public/bear-brand.webp";
 
 const ItemCard = ({ item, src, handleCardClick }) => {
   return (
     <div onClick={handleCardClick} className=" border shadow-md rounded-lg overflow-hidden cursor-pointer flex flex-col justify-between transition-all ease-in-out duration-300 hover:shadow-sky-200 hover:border-sky-200 w-full" title={item.name}>
       <div className="flex items-center justify-center flex-1 h-40">
-        <img src={src ? `http://localhost:5000/${src}` : itemImage} alt="" className="w-full object-cover" />
+        <img src={`http://localhost:5000/${src}`} alt="" className="w-full object-cover" />
       </div>
       <div className="p-2 pb-4 whitespace-nowrap">
         <div className="text-gray-700 font-medium text-sm max-w-[15ch] overflow-hidden overflow-ellipsis">
